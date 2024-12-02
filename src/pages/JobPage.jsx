5762d18b683496a8c2fa467b0c7853299b302292
MaratFa <evolution99@rambler.ru>
2024-12-03 01:00:12 +0300

    Edited

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,17 +1,31 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from '../components/Spinner';
 
 const JobPage = () => {
+  const { id } = useParams();
   const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchJob = async () => {
-
+      try {
+        const res = await fetch(`/api/job/${id}`);
+        const data = await res.json();
+        setJob();
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
     }
 
     fetchJob();
   }, [])
 
-  return <div>JobPage</div>;
-};
+  return loading ? <Spinner /> : (
+    <h1>Job</h1>
+  )
+  };
 
 export default JobPage;
